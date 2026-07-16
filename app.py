0fd9f0412a33939a097ddf7afa7260e528b5aa4f
Francisco Amorim Silva <frankprimo@yahoo.com.br>
2026-07-16 03:42:39 -0300

    Corrige configuracao SQLite database reduzido

--- app.py
+++ app.py
@@ -24,11 +24,15 @@ app.config.update(
 database_url = os.getenv("DATABASE_URL")
 
 if database_url:
-    if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://"
+    )
+
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Shopee/database.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database_reduzido.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
